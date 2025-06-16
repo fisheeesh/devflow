357f84127923aab7a46393671be37408d98c8849
fisheeesh <swanphyo444@gmail.com>
2025-06-16 15:07:46 +0700

    chore: create authLayout & social auth form

---
+++ components/forms/social-auth-form.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button } from '../ui/button'
+import Image from 'next/image'
+
+export default function SocialAuthForm() {
+    const btnClass = 'background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5'
+
+    return (
+        <div className='mt-10 flex flex-wrap gap-2.5'>
+            <Button className={btnClass}>
+                <Image src='/icons/github.svg' alt='Github Logo' width={20} height={20} className='invert-colors mr-2.5 object-contain' />
+                <span>Log in with Github</span>
+            </Button>
+            <Button className={btnClass}>
+                <Image src='/icons/google.svg' alt='Google Logo' width={20} height={20} className='mr-2.5 object-contain' />
+                <span>Log in with Google</span>
+            </Button>
+        </div>
+    )
+}
