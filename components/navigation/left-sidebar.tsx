a2c41bc9fae083683a2c82074e43c6a081ae366f
fisheeesh <swanphyo444@gmail.com>
2025-06-17 16:56:08 +0700

    fix: adjusted leftside bar layout style

--- components/navigation/left-sidebar.tsx
+++ components/navigation/left-sidebar.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 
 export default function LeftSideBar() {
     return (
-        <section className='custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 h-screen flex flex-col justify-between overflow-y-auto border-r p-6 pt-32 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]'>
+        <section className='custom-scrollbar background-light900_dark200 gap-16 light-border sticky left-0 top-0 h-screen flex flex-col justify-between overflow-y-auto border-r p-6 pt-32 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]'>
             <div className="flex flex-col flex-1 gap-6">
                 <NavLinks />
             </div>
