33a8f02e6d0e8a8c5005187b03cf029393befe63
fisheeesh <swanphyo444@gmail.com>
2025-06-21 15:19:00 +0700

    feat: create interaction model

---
+++ database/interaction.model.ts
@@ -0,0 +1,25 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IInteraction {
+    user: Types.ObjectId,
+    action: string,
+    actionId: Types.ObjectId,
+    actionType: "question" | "answer"
+}
+
+const InteractionSchema = new Schema<IInteraction>({
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    //* It can be upvote, downvote, view, askQuestion or whatever
+    action: { type: String, required: true },
+    /**
+     * * It can be quesitonId if user view or answer specific question
+     * * It can be answerId or userId if they are viewing specific user details
+     */
+    actionId: { type: Schema.Types.ObjectId, required: true },
+    actionType: { type: String, enum: ['question', 'answer'], required: true }
+
+}, { timestamps: true });
+
+const Interaction = models?.Interaction || model<IInteraction>("Interaction", InteractionSchema)
+
+export default Interaction;
