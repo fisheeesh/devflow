1b090b1580ad13f74584024c770f04012ded56c5
fisheeesh <swanphyo444@gmail.com>
2025-06-25 17:30:24 +0700

    faet: implemented create & read user routes

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 interface Tag {
     _id: string;
     name: string;
@@ -18,4 +20,22 @@ interface Question {
     upvotes: number;
     answers: number;
     views: number;
-}   
+}
+
+type ActionResponse<T = null> = {
+    success: boolean,
+    data?: T,
+    error?: {
+        message: string,
+        details?: Record<string, string[]>,
+    },
+    status?: number
+}
+
+type SuccessResponse<T = null> = ActionResponse<T> & { success: true }
+
+type ErrorResponse = ActionResponse<undefined> & { success: false }
+
+type APIErrorResponse = NextResponse<ErrorResponse>
+
+type APIResponse<T = null> = NextResponse<SuccessResponse<T> | ErrorResponse>
