f82c56e2c26ba38a40fcde90094a92655565fcf2
fisheeesh <swanphyo444@gmail.com>
2025-06-26 00:42:42 +0700

    feat: implemented dynamic account routes

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,6 +1,6 @@
 import User from "@/database/user.model"
 import handleError from "@/lib/handlers/error"
-import { NotFoundError, ValidationError } from "@/lib/http-error"
+import { NotFoundError } from "@/lib/http-error"
 import dbConnect from "@/lib/mongoose"
 import { UserSchema } from "@/lib/validation"
 import { APIErrorResponse } from "@/types/global"
@@ -12,7 +12,7 @@ export async function GET(_: Request, { params }: { params: Promise<{ id: string
 
     try {
         await dbConnect()
-        const user = await User.findById(id)
+        const user = await User.findById(id) 
         if (!user) throw new NotFoundError('User')
 
         return NextResponse.json({ success: true, data: user }, { status: 200 })
