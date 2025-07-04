b72a892fd03ebc1bcaa282a995b05d97ecc61988
fisheeesh <swanphyo444@gmail.com>
2025-07-04 17:04:33 +0700

    feat: implemented getTags server actions

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,9 +1,20 @@
+import { getTags } from '@/lib/actions/tag.actions'
 import React from 'react'
 
-export default function TagsPage() {
+export default async function TagsPage() {
+    const { success, data, error } = await getTags({
+        page: 1,
+        pageSize: 2,
+        query: 'javascript',
+    })
+
+    const { tags } = data || {}
+
+    console.log('TAGS', JSON.stringify(tags, null, 2))
+
     return (
         <div>
-ga
+            ga
         </div>
     )
 }
