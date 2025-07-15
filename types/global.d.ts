38aab5b1ae9562384840fff49969a688a93804ed
fisheeesh <swanphyo444@gmail.com>
2025-07-15 18:56:57 +0700

    feat: implemented getAllUsers action

--- types/global.d.ts
+++ types/global.d.ts
@@ -64,3 +64,14 @@ interface Answer {
     upvotes: number,
     downvotes: number
 }
+
+interface User {
+    _id: string,
+    name: string,
+    username: string,
+    email: string,
+    bio?: string,
+    image?: string,
+    location?: string,
+    reputation?: number
+}
