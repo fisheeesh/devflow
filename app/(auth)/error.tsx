03e36ae5a3f66ec9503a768f1fccbc59f107d8ba
fisheeesh <swanphyo444@gmail.com>
2025-08-04 17:44:13 +0700

    chore: added error.tsx to auth layout

---
+++ app/(auth)/error.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import CatLottie from '@/components/cat';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function Error() {
+    const router = useRouter()
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/')
+        }, 3000)
+    }, [router])
+
+    return (
+        <div className='min-h-screen flex items-center text-sm px-8 md:text-base justify-center text-center flex-col'>
+            <CatLottie />
+            <p className='font-medium text-7xl'>500</p>
+            <h1 className='mb-3 max-w-sm mx-auto text-dark100_light900'>Something went wrong on our end. Please try refreshing the page or come back later.</h1>
+        </div>
+    );
+}
+;
+
+
+
